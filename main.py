4570f40f273f960b9d92770507aef06dd0faa05b
lilpacy <revivedtomorrow@gmail.com>
2023-02-24 06:53:42 +0900

    update to take off character limit

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ documents = SimpleDirectoryReader('data').load_data()
 index = GPTSimpleVectorIndex(
     documents=documents,
     prompt_helper=PromptHelper(
-        max_input_size=5000,  # LLM入力の最大トークン数
-        num_output=256,  # LLM出力のトークン数
-        chunk_size_limit=2000,  # チャンクのトークン数
-        max_chunk_overlap=-10,  # チャンクオーバーラップの最大トークン数
+        max_input_size=5000,
+        num_output=4096,  # converted to openai api davinci 003 max_tokens parameter (4096 is max)
+        chunk_size_limit=2000,
+        max_chunk_overlap=-68,  # adjust to num_output
     ))
 
 input = "GPT indexをインストールする手順は？"
