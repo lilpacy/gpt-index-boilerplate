053d8ad8382143f6a64afa13c3a90a4fb4829120
lilpacy <revivedtomorrow@gmail.com>
2023-02-24 05:05:37 +0900

    update to specify llm arguments

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from gpt_index import GPTSimpleVectorIndex, SimpleDirectoryReader
+from gpt_index import GPTSimpleVectorIndex, SimpleDirectoryReader, PromptHelper
 
 documents = SimpleDirectoryReader('data').load_data()
-index = GPTSimpleVectorIndex(documents)
+index = GPTSimpleVectorIndex(
+    documents=documents,
+    prompt_helper=PromptHelper(
+        max_input_size=5000,  # LLM入力の最大トークン数
+        num_output=256,  # LLM出力のトークン数
+        chunk_size_limit=2000,  # チャンクのトークン数
+        max_chunk_overlap=-10,  # チャンクオーバーラップの最大トークン数
+    ))
 
 input = "GPT indexをインストールする手順は？"
 result = index.query(input)
